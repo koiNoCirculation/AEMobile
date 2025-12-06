82353ae723d74b8a3b3d74f3545c53d0b89c3bf5
koiNoCirculation <belica.ly42@gmail.com>
2025-12-06 13:09:25 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ ! -d "build" ]; then
+if [[ ! -d "build" ]]; then
   mkdir -p build
 fi
 arch=`uname -m`
-os=`uname -a`
-if [ $os == 'Darwin' ]; then
-  if [ $arch == 'arm64' ]; then
+os=`uname`
+if [[ $os == 'Darwin' ]]; then
+  if [[ $arch == 'arm64' ]]; then
     url='https://nodejs.org/dist/v22.21.1/node-v22.21.1-darwin-arm64.tar.gz'
     d='node-v22.21.1-darwin-arm64'
   else
@@ -13,7 +13,7 @@ if [ $os == 'Darwin' ]; then
     d='node-v22.21.1-darwin-x64'
   fi
 else
-  if [ $arch == 'x86_64' ]; then
+  if [[ $arch == 'x86_64' ]]; then
     url='https://nodejs.org/dist/v22.21.1/node-v22.21.1-linux-x64.tar.xz'
     d='node-v22.21.1-linux-x64'
   else
@@ -21,7 +21,8 @@ else
     d='node-v22.21.1-linux-arm64'
   fi
 fi
-curl -o build/node.tar.xz && cd build && tar -xf build/node.tar.xz && cd ../
+echo `pwd`
+curl $url -o build/node.tar.xz && cd build && tar -xf node.tar.xz && cd ../
 git submodule update --init --recursive
 cd AEMonitor-Frontend && '../build/${d}/bin/npm install' && '../build/${d}/bin/npm run build'
 mkdir -p ../src/main/resources/static && cp -r build/* ../src/main/resources/static
